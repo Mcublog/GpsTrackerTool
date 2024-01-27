9dcfd95242a14d3680d61063f9490e9f757e6d8d
Viacheslav <slava.k@ks2corp.com>
2024-01-27 18:22:27 +0300

    claenup

--- src/tracker/protocol/cobs/message.py
+++ src/tracker/protocol/cobs/message.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import ctypes as ct
+from dataclasses import dataclass, field
 
 import serial
 from cobs import cobsr
@@ -9,6 +10,7 @@ from tracker.protocol.commands.reports import GetReportsV1
 
 DataPointer = ct.POINTER(ct.c_uint8)
 
+
 class CMessageHeader(ct.Structure):
     _pack_ = 1
     _fields_ = [
@@ -16,6 +18,8 @@ class CMessageHeader(ct.Structure):
         ('size', ct.c_uint32),
         ('version', ct.c_uint32),
     ]
+
+
 class CMessage(ct.Structure):
     _pack_ = 1
     _fields_ = [
@@ -26,45 +30,57 @@ class CMessage(ct.Structure):
     ]
 
 
+@dataclass
 class Message:
+    channel: int
+    # size with header
+    size: int = field(init=False)
+    version: int
+    data: bytes = field(default=b'')
 
-    @staticmethod
-    def len() -> int:
-        return ct.sizeof(CMessageHeader)
+    def __post_init__(self):
+        self.size = header_size() if self.data is None else len(
+            self.data) + header_size()
 
-    @staticmethod
-    def serialize(channel: int, version:int, data: bytes | None) -> bytes:
-        size = Message.len() if data is None else len(data) + Message.len()
-        msg = CMessage(channel=channel, size=size, version=version)
-        buffer = (ct.c_uint8 * size)()
-        ct.memmove(ct.addressof(buffer), ct.addressof(msg), Message.len())
-        if data is not None:
-            ct.memmove(ct.addressof(buffer) + Message.len(), data, len(data))
-        return cobsr.encode(bytes(buffer)) + b'\x00'
 
-    @staticmethod
-    def deserialize(raw: bytes) -> bytes:
-        # remove /x00
-        size = len(raw) - 1
-        decoded = cobsr.decode(raw[:size])
-        decoded += b'\x00' * (size - len(decoded))
-        cmsg = CMessageHeader.from_buffer_copy(decoded)
-        return decoded
+def serialize(msg: Message) -> bytes:
+    cmsg = CMessage(channel=msg.channel, size=msg.size, version=msg.version)
+    buffer = (ct.c_uint8 * msg.size)()
+    ct.memmove(ct.addressof(buffer), ct.addressof(cmsg), header_size())
+    if msg.data is not None:
+        ct.memmove(
+            ct.addressof(buffer) + header_size(), msg.data, len(msg.data))
+    return cobsr.encode(bytes(buffer)) + b'\x00'
+
+
+def deserialize(raw: bytes) -> Message:
+    # remove /x00
+    size = len(raw) - 1
+    decoded = cobsr.decode(raw[:size])
+    decoded += b'\x00' * (size - len(decoded))
+    cmsg = CMessageHeader.from_buffer_copy(decoded)
+    return Message(channel=cmsg.channel,
+                   version=cmsg.version,
+                   data=decoded[header_size():])
+
+
+def header_size() -> int:
+    return ct.sizeof(CMessageHeader)
 
 
 def main():
-    # raw =  b"\002\v\001\001\002\024\001\001\002\026\001\001\002\001\001\001\002\002\001\001\00"
-    # # \x02\x08\x01\x01\x01\x01\x01\x01\x00'
-    # # raw = b'\x02\x08\x01\x01\x02\x04\x01\x01\x00'
-    # v = Message.deserialize(raw)
+    # raw = b"\002\v\001\001\002\024\001\001\002\026\001\001\002\001\001\001\002\002\001\001\00"
+    # v = deserialize(raw)
     # print(v)
 
     port = "/dev/ttyS10"
     get_report = GetReportsV1()
-    msg = Message.serialize(channel=0, version=0, data=bytes(get_report))
+    msg = serialize(Message(channel=0, version=0, data=bytes(get_report)))
     with serial.Serial(port, timeout=0.5) as ser:
         ser.write(msg)
-        print(ser.read(100))
+        if (raw := ser.read_until(b'\x00')) != b'':
+            v = deserialize(raw)
+            print(v)
 
 
 if __name__ == "__main__":
